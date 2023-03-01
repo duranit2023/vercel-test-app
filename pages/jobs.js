1247f6f1a6621287261d96101b44b4b9d4635047
kusal98 <kusal@duranit.lk>
2023-03-01 10:35:03 +0530

    Componets and routes fix modules

--- pages/jobs.js
+++ pages/jobs.js
@@ -1,5 +1,5 @@
 import JobsTable from "@/components/JobsTable"
-import Navbar from "@/components/navbar"
+import Navbar from "@/components/Navbar"
 
 function jobs() {
     return (
--- pages/users.js
+++ pages/users.js
@@ -1,4 +1,4 @@
-import Navbar from "@/components/navbar"
+import Navbar from "@/components/Navbar"
 import UsersTable from "@/components/UsersTable"
 
 function users(params) {
