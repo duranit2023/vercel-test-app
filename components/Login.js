fbbe051842cc547991962db11074454c3962d31e
kusal98 <kusal@duranit.lk>
2023-03-01 10:00:01 +0530

    Login componet

---
+++ components/Login.js
@@ -0,0 +1,27 @@
+function login() {
+    return (
+        <>
+        <div className="form-body">
+            <div className="login-page">
+                <div className="form">
+                    <form  className="register-form">
+                    <input type="text" placeholder="name"/>
+                    <input type="password" placeholder="password"/>
+                    <input type="text" placeholder="email address"/>
+                    <button>create</button>
+                    <p className="message">Already registered? <a>Sign In</a></p>
+                    </form>
+                    <form action="/api/auth/[..nextauth]" className="login-form" method="post">
+                    <input type="text" placeholder="username"/>
+                    <input type="password" placeholder="password"/>
+                    <button>login</button>
+                    <p className="message">Not registered? <a>Create an account</a></p>
+                    </form>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default login
